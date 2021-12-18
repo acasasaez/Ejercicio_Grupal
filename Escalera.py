6007b03d13114bfb4b6eddd0084fe887ee79fed2
acasasaez <acasasae@myuax.com>
2021-12-18 12:37:13 +0100

    fin y mejora de código

--- Escalera.py
+++ Escalera.py
@@ -1,7 +1,8 @@
-n = int (input())
+n = int (input(" Introduce el número de escalones de tu escalera: "))
 for i in range (1):
     a = " "
     for j in range (0,n):
         a = a + " "
         for g in range (0, i+1):
             a = a + "#"
+            print (a)

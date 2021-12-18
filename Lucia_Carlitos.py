dd763d0167f1669f65232c431a38d3ad4ab4ad0d
acasasaez <acasasae@myuax.com>
2021-12-18 11:51:20 +0100

    modificaciones en las funciones para que sean más efectivas

--- Lucia_Carlitos.py
+++ Lucia_Carlitos.py
@@ -8,18 +8,20 @@ def claridad ():
     global carlitos
     a[0]= int(input("Introduzca la puntuación de Lucía"))
     b[0]=int(input("Introduzca la puntuación de Carlitos"))
-    if a [0]>100 or b[0]> 100 or a[0]<0 or b[0]<0:
+    while a [0]>100 or b[0]> 100 or a[0]<0 or b[0]<0:
         print ("Las puntuaciones asignadas deben estar entre 0 y 100")
-    else:
-        if a[0]>b[0]:
+        a[0]= int(input("Introduzca la puntuación de Lucía"))
+        b[0]=int(input("Introduzca la puntuación de Carlitos"))
+    
+    if a[0]>b[0]:
            lucia+= 1
            print ("Lucía tiene", lucia,"puntos.")
            print("Carlitos tiene", carlitos, "puntos.")
-        elif a[0] < b[0]:
+    elif a[0] < b[0]:
             carlitos += 1
             print ("Lucía tiene", lucia,"puntos.")
             print("Carlitos tiene", carlitos, "puntos.")
-        else:
+    else:
             print ("EMPATE")
             print ("Lucía tiene", lucia,"puntos.")
             print("Carlitos tiene", carlitos, "puntos.")
@@ -30,18 +32,20 @@ def originalidad ():
     global carlitos 
     a[1] = int(input("Introduzca la puntuación de Lucía"))
     b [1]= int (input ("Introduzca la puntuación de Carlitos "))
-    if a[1]> 100 or b[1]> 100 or a[1]< 0 or b[1]< 0:
+    while a[1]> 100 or b[1]> 100 or a[1]< 0 or b[1]< 0:
         print ("Las puntuaciones asignadas deben estar comprendidas entre 0 y 100")
-    else:
-        if a[1]>b[1]:
+        a[1] = int(input("Introduzca la puntuación de Lucía"))
+        b [1]= int (input ("Introduzca la puntuación de Carlitos "))
+    
+    if a[1]>b[1]:
             lucia += 1
             print ("Lucía tiene",lucia,"puntos.")
             print("Carlitos tiene",carlitos,"puntos.")
-        elif a[1] < b [1]:
+    elif a[1] < b [1]:
             carlitos +=1
             print ("Lucía tiene",lucia,"puntos.")
             print("Carlitos tiene",carlitos,"puntos.")
-        else:
+    else:
             print ("EMPATE")
             print ("Lucía tiene",lucia,"puntos.")
             print("Carlitos tiene",carlitos,"puntos.")
@@ -52,18 +56,20 @@ def dificultad ():
     global carlitos
     a[2] = int (input ("Introduzca la puntuación de Lucía"))
     b[2]= int(input("Introduzca la puntuación de Carlitos"))
-    if a[1]> 100 or b[1]> 100 or a[1]< 0 or b[1]< 0:
-        print ("Las puntuaciones asignadas deben estar emtre 0 y 100")
-    else:
-        if a [2]> b[2]:
+    while a[2]> 100 or b[2]> 100 or a[2]< 0 or b[2]< 0:
+        print ("Las puntuaciones asignadas deben estar entre 0 y 100")
+        a[2] = int (input ("Introduzca la puntuación de Lucía"))
+        b[2]= int(input("Introduzca la puntuación de Carlitos"))
+    
+    if a [2]> b[2]:
             lucia +=1
             print ("Lucía tiene", lucia, "puntos.")
             print ("Carlitos tiene", carlitos, "puntos")
-        elif a[2] < b[2]:
+    elif a[2] < b[2]:
             carlitos += 1
             print ("Lucía tiene", lucia, "puntos.")
             print ("Carlitos tiene", carlitos, "puntos")
-        else: 
+    else: 
             print ("EMPATE")
             print ("Lucía tiene", lucia, "puntos.")
             print ("Carlitos tiene", carlitos, "puntos")

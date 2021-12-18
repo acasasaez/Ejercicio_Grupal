931b81777441dc84269929738cf4042805f49bec
guerramorante <mmorante@myuax.com>
2021-12-18 12:02:24 +0100

    Importamos y definimos variable

--- Suma_matriz_simple.py
+++ Suma_matriz_simple.py
@@ -4,5 +4,23 @@ import random
 import re
 import sys
 
-def aVeryBigSum(ar):
-    
+def simpleArraySum(matriz):
+    suma = 0
+    for fila in matriz:
+        for n in fila:
+            suma += n
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'] + 'Suma_matriz_simple' , 'w')
+matriz_count = int(input().strip())
+print("Escribe la lista de números que desea operar ")
+matriz = list(map(int, input().rstrip().split()))
+
+
+result = simpleArraySum(matriz)
+
+fptr.write(str(result) + '\n')
+
+fptr.close()
+
+

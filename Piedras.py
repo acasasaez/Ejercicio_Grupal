b3a1eb33c41e3255a17d05223219d42a4b4d798c
acasasaez <acasasae@myuax.com>
2021-12-18 18:22:05 +0100

    primera función

--- Piedras.py
+++ Piedras.py
@@ -1,1 +1,10 @@
 import os
+def gameOfStrones ():
+    ganador = " "
+    if (jugadaoptima(n)!=0):
+        ganador= "P1 es el ganador"
+    else:
+        ganador = "P2 es el ganador"
+    return ganador
+
+

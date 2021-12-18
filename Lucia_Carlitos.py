25229cf33265b35ed2367c3594b7ddcc67a16e48
acasasaez <acasasae@myuax.com>
2021-12-18 10:49:25 +0100

    Creamos la función que evalúe la claridad del problema

--- Lucia_Carlitos.py
+++ Lucia_Carlitos.py
@@ -1,4 +1,27 @@
 lucia = 0
 carlitos = 0
-a = [0,1,2]
-b= [0,1,2]
+
+def claridad ():
+    a = [0,1,2]
+    b= [0,1,2]
+    global lucia
+    global carlitos
+    a[0]= int(input("Introduzca la puntuación de Lucía"))
+    b[0]=int(input("Introduzca la puntuación de Carlitos"))
+    if a [0]>100 or b[0]> 100 or a[0]<0 or b[0]<0:
+        print ("Las puntuaciones asignadas deben estar entre 0 y 100")
+    else:
+        if a[0]>b[0]:
+           lucia+= 1
+           print ("Lucía tiene", lucia,"puntos.")
+           print("Carlitos tiene", carlitos, "puntos.")
+        elif a[0] < b[0]:
+            carlitos += 1
+            print ("Lucía tiene", lucia,"puntos.")
+            print("Carlitos tiene", carlitos, "puntos.")
+        else:
+            print ("EMPATE")
+            print ("Lucía tiene", lucia,"puntos.")
+            print("Carlitos tiene", carlitos, "puntos.")
+
+

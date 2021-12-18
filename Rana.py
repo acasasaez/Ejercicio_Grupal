3838825c0705fe68896ed82caf73699b4a595727
acasasaez <acasasae@myuax.com>
2021-12-18 16:04:45 +0100

    creamos funciones 1

--- Rana.py
+++ Rana.py
@@ -3,7 +3,7 @@ import os
 import random
 import re
 import sys 
-class coordenadas:
+class Coordenadas:
     def __int__ (self, x,y):
         self.x = x
         self.y = y
@@ -14,5 +14,16 @@ class coordenadas:
             return False
 class Tunel:
     def __init__(self,x1,x2,y1,y2):
-        self.extremo1 = coordenadas(x1,y1)
-        self.extremo2 = coordenadas (x2,y2)
+        self.extremo1 = Coordenadas(x1,y1)
+        self.extremo2 = Coordenadas (x2,y2)
+def BuscaTunel (casillax, casillay, tuneles):
+    coordenadas = Coordenadas(casillax, casillay)
+    for t in tuneles:
+        if t.extremo1.comparate(casillax,casillay)==True:
+            coordenadas.x = t.extremo2.x
+            coordenadas.y = t.extremo2.y
+        elif t.extremo2.comparate(casillax, casillay):
+            coordenadas.x=t.extremo1.x
+            coordenadas.y = t.extremo1.y
+    return coordenadas
+    
